531db4211ade4e886c81909cb3f6ade267b20339
ruy <7leckler@gmail.com>
2025-03-03 16:38:08 -0300

    feat: adicionando 2 formas p mockar a camada de persistencia

--- src/test/java/com/backend/makeUrTasks/makeUrTasks/repository/TaskRepositoryTest.java
+++ src/test/java/com/backend/makeUrTasks/makeUrTasks/repository/TaskRepositoryTest.java
@@ -2,10 +2,14 @@ package com.backend.makeUrTasks.makeUrTasks.repository;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("TaskTestE2E Model Tests")
+@SpringBootTest
+
 public class TaskRepositoryTest {
 
   @Test
